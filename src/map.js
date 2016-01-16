8591a4b5e850f26f322f8a75dbc96a73fea21f6c
alpha-beta-soup <richard.m.law@gmail.com>
2016-01-17 01:50:20 +1300

    Moved things

--- src/map.js
+++ src/map.js
@@ -1,6 +1,6 @@
 var countriesJson;
 
-countriesJson = $.getJSON('https://github.com/alpha-beta-soup/world-flags-in-context/blob/master/data/ne-v2.0.0-countries-geojson/ne-countries-50m.json', function(json) {
+countriesJson = $.getJSON('../src/data/ne-v2.0.0-countries-geojson/ne-countries-50m.json', function(json) {
   var get_icon, map, onEachFeature;
   map = L.map('map').setView([51.505, -0.09], 3);
   get_icon = function(iso) {
@@ -8,7 +8,7 @@ countriesJson = $.getJSON('https://github.com/alpha-beta-soup/world-flags-in-con
     if (iso !== 'nz') {
       url = "http://www.geonames.org/flags/x/" + iso + ".gif";
     } else {
-      url = "../lockwood.png";
+      url = "./src/lockwood.png";
     }
     flag_icon = L.icon({
       iconUrl: url,
@@ -21,17 +21,53 @@ countriesJson = $.getJSON('https://github.com/alpha-beta-soup/world-flags-in-con
     cc = feature.properties.iso_a2.toLowerCase();
     layer.bindPopup("http://www.geonames.org/flags/x/" + cc + ".gif");
     flag_location = turf.centroid(feature).geometry.coordinates;
-    if (flag_location == null) {
+    if ((flag_location == null) || cc === '-99') {
       return;
     }
     if (cc === 'nz') {
       flag_location[0] += 23;
+    } else if (cc === 'us') {
+      flag_location[0] += 25;
+      flag_location[1] -= 10;
+    } else if (cc === 'ca') {
+      flag_location[0] -= 15;
+      flag_location[1] -= 10;
+    } else if (cc === 'hr') {
+      flag_location[1] += 0.8;
+    } else if (cc === 'nl') {
+      flag_location[0] += 7;
+      flag_location[1] += 4;
+    } else if (cc === 'fr') {
+      flag_location[0] += 10;
+      flag_location[1] += 12;
+    } else if (cc === 'es') {
+      flag_location[0] += 1.5;
+      flag_location[1] += 1.5;
+    } else if (cc === 'th') {
+      flag_location[1] += 2;
+    } else if (cc === 'la') {
+      flag_location[0] -= 1.5;
+      flag_location[1] += 2;
+    } else if (cc === 'vn') {
+      flag_location[1] += 5;
+    } else if (cc === 'zm') {
+      flag_location[1] -= 2;
+      flag_location[0] -= 3;
+    } else if (cc === 'mz') {
+      flag_location[1] -= 1;
+    } else if (cc === 'fj') {
+      flag_location[0] += 121;
+    } else if (cc === 'ps') {
+      flag_location[1] += 0.5;
+      flag_location[0] += 0.2;
+    } else if (cc === 'il') {
+      flag_location[0] -= 0.4;
     }
     flag_icon = get_icon(cc);
     return L.marker([flag_location[1], flag_location[0]], {
       icon: flag_icon,
       opacity: 1
-    }).addTo(map);
+    }).addTo(map).bindPopup(feature.properties.name + " (" + feature.properties.iso_a2 + ")");
   };
   return L.geoJson(json, {
     onEachFeature: onEachFeature,
